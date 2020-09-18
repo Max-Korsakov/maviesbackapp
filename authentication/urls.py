da282acaf8d73a0d12ee0c74b00b9f87ec0e9553
Max Korsakov <Maksim_Karsakou1epam.com>
2020-09-18 15:23:13 +0300

    add token refresh

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+)
 
 from authentication.views import (
     register_user,
@@ -8,7 +11,8 @@ from authentication.views import (
 )
 
 urlpatterns = [
-    path('register/', register_user, name="register"),
-    path('login/', login_user, name="login"),
+    path('register', register_user, name="register"),
+    path('login', login_user, name="login"),
+    path('login/refresh', TokenRefreshView.as_view(), name="refresh"),
     path('delete', delete_user, name="delete")
 ]
